a0154c17e44ea2b6d32f07ddeb8d484e354fe9bd
Brian Wilmeth <bwil@lightbearsoftware.com>
2025-12-28 13:55:27 -0500

    changed base class names

--- Source/StuffedUp/Private/CharacterBase.cpp
+++ Source/StuffedUp/Private/CharacterBase.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "BearCharacterBase.h"
+#include "CharacterBase.h"
 
 // Sets default values
-ABearCharacterBase::ABearCharacterBase()
+ACharacterBase::ACharacterBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -12,21 +12,21 @@ ABearCharacterBase::ABearCharacterBase()
 }
 
 // Called when the game starts or when spawned
-void ABearCharacterBase::BeginPlay()
+void ACharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
 // Called every frame
-void ABearCharacterBase::Tick(float DeltaTime)
+void ACharacterBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
 // Called to bind functionality to input
-void ABearCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void ACharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
